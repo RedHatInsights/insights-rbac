1037a99b2e83158d0d82f7a4e85af25cffcfd596
Chris Hambridge <chambrid@redhat.com>
2019-08-20 14:40:34 -0400

    Use threaded gunicorn workers for improved performance.

--- rbac/gunicorn.py
+++ rbac/gunicorn.py
@@ -1,5 +1,8 @@
 """Gunicorn configuration file."""
 import multiprocessing
+import os
 
 bind = 'unix:/var/run/rbac/gunicorn.sock'
-workers = multiprocessing.cpu_count() * 2 + 1
+cpu_resources = int(os.environ.get('POD_CPU_LIMIT', multiprocessing.cpu_count()))
+workers = cpu_resources * 2
+threads = 10
