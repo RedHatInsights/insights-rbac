feefd6e80dd236ed64e6e6e7bbdbc742218b268a
Kyle Lape <klape@redhat.com>
2020-02-18 17:24:25 -0600

    Clean up DB env config

--- rbac/rbac/database.py
+++ rbac/rbac/database.py
@@ -22,38 +22,17 @@ from django.conf import settings
 
 from .env import ENVIRONMENT
 
-# pylint: disable=invalid-name
-engines = {
-    'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'tenant_schemas.postgresql_backend',
-    'mysql': 'django.db.backends.mysql',
-}
-
 
 def config():
     """Database config."""
-    service_name = ENVIRONMENT.get_value('DATABASE_SERVICE_NAME',
-                                         default='').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(ENVIRONMENT.get_value('DATABASE_ENGINE'),
-                             engines['postgresql'])
-    else:
-        engine = engines['postgresql']
-
-    name = ENVIRONMENT.get_value('DATABASE_NAME', default=None)
-
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
 
     db_obj = {
-        'ENGINE': engine,
-        'NAME': name,
+        'ENGINE': 'tenant_schemas.postgresql_backend',
+        'NAME': ENVIRONMENT.get_value('DATABASE_NAME', default=None),
         'USER': ENVIRONMENT.get_value('DATABASE_USER', default=None),
         'PASSWORD': ENVIRONMENT.get_value('DATABASE_PASSWORD', default=None),
-        'HOST': ENVIRONMENT.get_value('{}_SERVICE_HOST'.format(service_name),
-                                      default=None),
-        'PORT': ENVIRONMENT.get_value('{}_SERVICE_PORT'.format(service_name),
-                                      default=None),
+        'HOST': ENVIRONMENT.get_value('DATABASE_HOST', default=None),
+        'PORT': ENVIRONMENT.get_value('DATABASE_PORT', default=None),
     }
 
     database_cert = ENVIRONMENT.get_value('DATABASE_SERVICE_CERT', default=None)
