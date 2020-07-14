998996db725a4683c1e4372224fa332610d7069f
Keith Walsh <kwalsh@redhat.com>
2020-07-14 08:43:52 -0400

    Add tests for Permission model and view

---
+++ tests/management/permission/test_view.py
@@ -0,0 +1,159 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Test the permission viewset."""
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from tenant_schemas.utils import tenant_context
+
+from api.models import User
+from management.models import Permission
+from tests.identity_request import IdentityRequest
+
+
+class PermissionViewsetTests(IdentityRequest):
+    """Test the permission viewset."""
+
+    def setUp(self):
+        """Set up the permission viewset tests."""
+        super().setUp()
+        request = self.request_context["request"]
+        user = User()
+        user.username = self.user_data["username"]
+        user.account = self.customer_data["account_id"]
+        request.user = user
+
+        self.display_fields = {"app", "resource", "operation", "permission"}
+
+        with tenant_context(self.tenant):
+            self.permissionA = Permission.objects.create(permission="rbac:roles:read")
+            self.permissionB = Permission.objects.create(permission="rbac:*:*")
+            self.permissionC = Permission.objects.create(permission="acme:*:*")
+            self.permissionD = Permission.objects.create(permission="acme:*:write")
+
+    def tearDown(self):
+        """Tear down permission viewset tests."""
+        with tenant_context(self.tenant):
+            Permission.objects.all().delete()
+
+    def test_read_permission_list_success(self):
+        """Test that we can read a list of permissions."""
+        url = reverse("permission-list")
+        client = APIClient()
+        response = client.get(url, **self.headers)
+
+        # three parts in response: meta, links and data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        for keyname in ["meta", "links", "data"]:
+            self.assertIn(keyname, response.data)
+        self.assertIsInstance(response.data.get("data"), list)
+        self.assertEqual(len(response.data.get("data")), 4)
+
+        for perm in response.data.get("data"):
+            self.assertIsNotNone(perm.get("app"))
+            self.assertIsNotNone(perm.get("resource"))
+            self.assertIsNotNone(perm.get("operation"))
+            self.assertIsNotNone(perm.get("permission"))
+            self.assertEqual(self.display_fields, set(perm.keys()))
+
+    def test_read_permission_list_application_filter(self):
+        """Test that we can filter a list of permissions by application."""
+        url = reverse("permission-list")
+        url = f"{url}?application=rbac"
+        client = APIClient()
+        response = client.get(url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("data")), 2)
+
+    def test_read_permission_list_resource_filter(self):
+        """Test that we can filter a list of permissions by resource."""
+        url = reverse("permission-list")
+        url = f"{url}?resource=roles"
+        client = APIClient()
+        response = client.get(url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("data")), 1)
+        self.assertEqual(response.data.get("data")[0].get("permission"), self.permissionA.permission)
+
+    def test_read_permission_list_operation_filter(self):
+        """Test that we can filter a list of permissions by operation."""
+        url = reverse("permission-list")
+        url = f"{url}?operation=read"
+        client = APIClient()
+        response = client.get(url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("data")), 1)
+        self.assertEqual(response.data.get("data")[0].get("permission"), self.permissionA.permission)
+
+    def test_read_permission_list_permission_filter(self):
+        """Test that we can filter a list of permissions by permission."""
+        url = reverse("permission-list")
+        url = f"{url}?permission=rbac:*:*"
+        client = APIClient()
+        response = client.get(url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("data")), 1)
+        self.assertEqual(response.data.get("data")[0].get("permission"), self.permissionB.permission)
+
+    def test_get_list_is_the_only_valid_method(self):
+        """Test GET on /permissions/ is the only valid method."""
+        url = reverse("permission-list")
+        client = APIClient()
+        response = client.post(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = client.put(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = client.delete(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class PermissionViewsetTestsNonAdmin(IdentityRequest):
+    """Test the permission viewset."""
+
+    def setUp(self):
+        """Set up the permission viewset tests."""
+        super().setUp()
+
+        self.user_data = self._create_user_data()
+        self.customer = self._create_customer_data()
+        self.request_context = self._create_request_context(self.customer, self.user_data, is_org_admin=False)
+
+        request = self.request_context["request"]
+        self.headers = request.META
+
+        with tenant_context(self.tenant):
+            self.permission = Permission.objects.create(permission="rbac:roles:read")
+            self.permission.save()
+
+    def tearDown(self):
+        """Tear down permission viewset tests."""
+        with tenant_context(self.tenant):
+            Permission.objects.all().delete()
+
+    def test_read_permission_list_fail(self):
+        """Test that we can not read a list of permissions as a non-admin."""
+        url = reverse("permission-list")
+        client = APIClient()
+        response = client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
