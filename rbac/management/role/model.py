8321af3c2ca5a4f686efc4ea60f1b6329ac4d4e7
Chris Mitchell <cmitchel@redhat.com>
2020-07-27 14:21:31 -0400

    Ensure that system roles can have updated display_names
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- rbac/management/role/model.py
+++ rbac/management/role/model.py
@@ -55,7 +55,7 @@ class Role(models.Model):
 
     def save(self, *args, **kwargs):
         """Ensure that display_name is populated on save."""
-        if not self.display_name or self.display_name != self.name:
+        if not self.display_name or (self.display_name != self.name and not self.system):
             self.display_name = self.name
         super(Role, self).save(*args, **kwargs)
 
