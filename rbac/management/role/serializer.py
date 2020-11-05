ae2a0c5ec54d4a168eb0715eef742d85d9aad746
Chris Mitchell <cmitchel@redhat.com>
2020-11-05 17:30:08 -0500

    Add validation to attributeFilter operations
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- rbac/management/role/serializer.py
+++ rbac/management/role/serializer.py
@@ -24,11 +24,23 @@ from rest_framework.validators import UniqueValidator
 
 from .model import Access, ResourceDefinition, Role
 
+ALLOWED_OPERATIONS = ['in', 'equal']
+
 
 class ResourceDefinitionSerializer(serializers.ModelSerializer):
     """Serializer for the ResourceDefinition model."""
 
     attributeFilter = serializers.JSONField()
+
+    def validate_attributeFilter(self, value):
+        "Validate the given attributeFilter"
+        op = value.get("operation")
+        if op and op not in ALLOWED_OPERATIONS:
+            key = "format"
+            message = f'attributeFilter operation must be one of {ALLOWED_OPERATIONS}'
+            error = {key: [_(message)]}
+            raise serializers.ValidationError(error)
+        return value
 
     class Meta:
         """Metadata for the serializer."""
