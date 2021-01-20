243b57a20ea2c1da87fe4292a2df9b19e1157efd
Kyle Lape <klape@redhat.com>
2021-01-20 17:12:25 -0600

    Fail PR check if tests fail

--- pr_check.sh
+++ pr_check.sh
@@ -71,13 +71,19 @@ pid=$!
 
 tox -r
 
+result=$?
+
 kill $pid
 bonfire namespace release $NAMESPACE
 
-# Need to make a dummy results file to make tests pass
-mkdir -p artifacts
-cat << EOF > artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
+if [ $result != 0 ]; then
+    exit $result
+else
+    # Need to make a dummy results file to make tests pass
+    mkdir -p artifacts
+    cat << EOF > artifacts/junit-dummy.xml
+    <testsuite tests="1">
+        <testcase classname="dummy" name="dummytest"/>
+    </testsuite>
 EOF
+fi
