afcd68319161f2e1c7e84dddd719e873e971109e
astrozzc <astrozzc@gmail.com>
2021-02-10 16:27:33 -0500

    Return only assigned permission for cross account principal
    For cross account principals, they have their own group and permissions.
    Do not return the platform default permissions for them.

--- rbac/management/utils.py
+++ rbac/management/utils.py
@@ -21,9 +21,12 @@ from uuid import UUID
 
 from django.core.exceptions import PermissionDenied
 from django.utils.translation import gettext as _
-from management.models import Group, Principal
+from management.models import Group, Principal, Role
 from management.principal.proxy import PrincipalProxy
 from rest_framework import serializers, status
+from tenant_schemas.utils import tenant_context
+
+from api.models import CrossAccountRequest, Tenant
 
 
 USERNAME_KEY = "username"
@@ -118,6 +121,8 @@ def groups_for_principal(principal, **kwargs):
         assigned_group_set = assigned_group_set.prefetch_related(prefetch_lookups)
         platform_default_group_set = platform_default_group_set.prefetch_related(prefetch_lookups)
 
+    if principal.cross_account:
+        return set(assigned_group_set)
     return set(assigned_group_set | platform_default_group_set)
 
 
@@ -129,6 +134,8 @@ def policies_for_principal(principal, **kwargs):
 
 def roles_for_principal(principal, **kwargs):
     """Gathers all roles for a principal."""
+    if principal.cross_account:
+        return roles_for_cross_account_principal(principal)
     policies = policies_for_principal(principal, **kwargs)
     return roles_for_policies(policies)
 
@@ -189,3 +196,14 @@ def validate_limit_and_offset(query_params):
             "status": str(status.HTTP_400_BAD_REQUEST),
         }
         return {"errors": [error]}
+
+
+def roles_for_cross_account_principal(principal):
+    """Return roles for cross account principals."""
+    target_account, user_id = principal.username.split("-")
+    with tenant_context(Tenant.objects.get(schema_name="public")):
+        cross_account_request = CrossAccountRequest.objects.filter(
+            target_account=target_account, user_id=user_id, status="approved"
+        ).first()
+        role_names = list(cross_account_request.roles.all().values_list("name", flat=True))
+    return Role.objects.filter(name__in=role_names)
