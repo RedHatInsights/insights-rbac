6dfcf3957ced19259a32fd03f35798ad8299bc26
Brandon Tweed <btweed@redhat.com>
2021-05-19 12:20:20 -0500

    Add a ClowdJobInvocation for IQE smoke tests

--- pr_check.sh
+++ pr_check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-APP_NAME="rbac"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="rbac"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/rbac"  # the image location on quay
+export APP_NAME="rbac"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="rbac"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/rbac"  # the image location on quay
 
-IQE_PLUGINS="rbac"  # name of the IQE plugin for this APP
-IQE_MARKER_EXPRESSION="rbac_smoke"  # This is the value passed to pytest -m
-IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+export IQE_PLUGINS="rbac"  # name of the IQE plugin for this APP
+export IQE_MARKER_EXPRESSION="rbac_smoke"  # This is the value passed to pytest -m
+export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+
+find . -name cdappconfig.json
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -14,5 +16,18 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 
+# Deploy ephemeral db
+source $CICD_ROOT/deploy_ephemeral_db.sh
+
+# Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
+export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
+
+# check if NAMESPACE is set
+[ -z "$NAMESPACE" ] && exit 1
+oc get secret rbac -o json -n $NAMESPACE
+
+# Run unit tests
 source $APP_ROOT/unit_test.sh
 
+# Smoke tests
+source $APP_ROOT/smoke_test.sh
--- unit_test.sh
+++ unit_test.sh
@@ -1,15 +1,9 @@
-# Deploy ephemeral db
-
-source $CICD_ROOT/deploy_ephemeral_db.sh
-
-# Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
-export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
-
 python3 -m venv app-venv
 . app-venv/bin/activate
 pip install --upgrade pip setuptools wheel pipenv tox psycopg2-binary
 tox -r
 result=$?
+deactivate
 
 # TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
 mkdir -p $WORKSPACE/artifacts
@@ -18,5 +12,3 @@ cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
     <testcase classname="dummy" name="dummytest"/>
 </testsuite>
 EOF
-
-exit $result
