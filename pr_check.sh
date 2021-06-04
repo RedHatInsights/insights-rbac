78d74146d765338736c9ad5b5fb6701496aef6fa
Brandon Tweed <btweed@redhat.com>
2021-06-04 15:31:19 -0500

    Bring sanity back to the land

--- pr_check.sh
+++ pr_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-APP_NAME="rbac"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="rbac"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/rbac"  # the image location on quay
+export APP_NAME="rbac"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="rbac"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export IMAGE="quay.io/cloudservices/rbac"  # the image location on quay
 
-IQE_PLUGINS="rbac"  # name of the IQE plugin for this APP
-IQE_MARKER_EXPRESSION="rbac_smoke"  # This is the value passed to pytest -m
-IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+export IQE_PLUGINS="rbac"  # name of the IQE plugin for this APP
+export IQE_MARKER_EXPRESSION="rbac_smoke"  # This is the value passed to pytest -m
+export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -14,11 +14,14 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 
-# source $APP_ROOT/unit_test.sh
+# Deploy ephemeral db
+source $CICD_ROOT/deploy_ephemeral_db.sh
+
+# Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
+export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
+
+# Run unit tests
+source $APP_ROOT/unit_test.sh
 
 # Smoke tests
-
-# Env vars defined by caller:
-export IQE_PLUGINS="rbac"
-export IQE_MARKER_EXPRESSION="rbac_smoke"
 source $CICD_ROOT/smoke_test.sh
--- unit_test.sh
+++ unit_test.sh
@@ -1,10 +1,3 @@
-# Deploy ephemeral db
-
-source $CICD_ROOT/deploy_ephemeral_db.sh
-
-# Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
-export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
-
 python3 -m venv app-venv
 . app-venv/bin/activate
 pip install --upgrade pip setuptools wheel pipenv tox psycopg2-binary
@@ -18,5 +11,3 @@ cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
     <testcase classname="dummy" name="dummytest"/>
 </testsuite>
 EOF
-
-exit $result
