2eb3507cd203cecf83c76d9f14a79cd52b59ea50
Brandon Tweed <btweed@redhat.com>
2021-06-04 14:08:51 -0500

    Experiment: force unit tests not to run

--- pr_check.sh
+++ pr_check.sh
@@ -14,7 +14,7 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
 
-source $APP_ROOT/unit_test.sh
+# source $APP_ROOT/unit_test.sh
 
 # Smoke tests
 
