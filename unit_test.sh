0ca544503746b5327bfe21f3a2c923db89812a5a
bholifie <bholifie@redhat.com>
2021-06-10 16:14:05 -0500

    fix venv issues

--- unit_test.sh
+++ unit_test.sh
@@ -1,6 +1,19 @@
+
+# Deploy ephemeral db
+source $CICD_ROOT/deploy_ephemeral_db.sh
+
+# Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
+export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
+
+# check if NAMESPACE is set
+[ -z "$NAMESPACE" ] && exit 1
+oc get secret rbac -o json -n $NAMESPACE
+
+
 python3 -m venv app-venv
 . app-venv/bin/activate
 pip install --upgrade pip setuptools wheel pipenv tox psycopg2-binary
+pip install crc-bonfire==v1.4.0b0
 tox -r
 result=$?
 deactivate
