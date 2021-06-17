3216bd57bc7651efbfe6d582258caa854a32dcb2
Brandon Tweed <btweed@redhat.com>
2021-06-17 10:12:54 -0500

    ok, fine, egrep then

--- smoke_test.sh
+++ smoke_test.sh
@@ -41,7 +41,7 @@ while [ $SECONDS -lt $end ]; do
     search_criteria="${job_name}"'-[a-z,A-Z,0-9]{5}$'
     echo "Search criteria regex: $search_criteria"
     oc get events -n $NAMESPACE
-    pod=$(oc get events -n $NAMESPACE | grep -o $search_criteria)
+    pod=$(oc get events -n $NAMESPACE | egrep -o $search_criteria | sort -u)
     echo "pod is $pod"
     if [ -n "$pod" ]; then
       running=true
