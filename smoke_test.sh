ea9dfc79954438b1d6c1e0c4282bb2ec893ec296
bholifie <bholifie@redhat.com>
2021-07-07 14:41:30 -0500

    use specific name

--- smoke_test.sh
+++ smoke_test.sh
@@ -5,60 +5,21 @@
 #IQE_FILTER_EXPRESSION="something AND something_else" -- pytest filter, can be "" if no filter desired
 #NAMESPACE="mynamespace" -- namespace to deploy iqe pod into, can be set by 'deploy_ephemeral_env.sh'
 
+IQE_CJI_NAME="rbac-smoke-tests"
 
 function kill_port_fwd {
     echo "Caught signal, kill port forward"
     if [ ! -z "$PORT_FORWARD_PID" ]; then kill $PORT_FORWARD_PID; fi
 }
 
-# # The CJI var name will need to be exported in the main pr_check.sh
-# oc apply -n $NAMESPACE -f $APP_ROOT/$IQE_CJI_PATH
-
-# job_name=$APP_NAME-smoke-tests-iqe
-# found=false
-# end=$((SECONDS+60))
-
-# echo "Waiting for Job $job_name to appear"
-
-# while [ $SECONDS -lt $end ]; do
-#     if `oc get job $job_name -n $NAMESPACE >/dev/null 2>&1`; then
-#         found=true
-#         break
-#     fi
-#     sleep 1
-# done
-
-# if [ "$found" == "false" ] ; then
-#     echo "Job $job_name failed to appear"
-#     exit 1
-# fi
-
-# echo "Waiting for Job $job_name to be running"
-# running=false
-
-# # The jq magic will find all running pods in the ns and regex on the app name
-# # Loop over for SECONDS and send back the pod's name once found
-# while [ $SECONDS -lt $end ]; do
-#     pod=$(oc get pods -n $NAMESPACE -o json | jq -r --arg JOB $job_name '.items[] | select(.status.phase=="Running") | select(.metadata.name|test($JOB)) .metadata.name')
-#     if [[ -n $pod ]]; then
-#         running=true
-#         break
-#     fi
-#     sleep 5
-# done
-
-# if [ "$running" == "false" ] ; then
-#     echo "Job $job_name failed to start"
-#     exit 1
-# fi
-pod=$(bonfire deploy-iqe-cji rbac -m "rbac_smoke" -e "clowder_smoke" -n $NAMESPACE)
+pod=$(bonfire deploy-iqe-cji rbac -m "rbac_smoke" -e "clowder_smoke" --cji-name $IQE_CJI_NAME -n $NAMESPACE)
 
 # Pipe logs to background to keep them rolling in jenkins
 oc logs -n $NAMESPACE $pod -f &
 
 # Wait for the job to Complete or Fail before we try to grab artifacts
 # condition=complete does trigger when the job fails
-oc wait --timeout=$IQE_CJI_TIMEOUT --for=condition=Complete -n $NAMESPACE job/$job_name
+oc wait --timeout=$IQE_CJI_TIMEOUT --for=condition=Complete -n $NAMESPACE job/$IQE_CJI_NAME
 
 # Get the minio client
 curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
