46c1c6fe337803ae713632734c3cda5d9c930dcb
Matt Holder <maholder@redhat.com>
2022-03-31 15:29:14 -0400

    linting

--- rbac/api/models.py
+++ rbac/api/models.py
@@ -26,8 +26,8 @@ class Tenant(models.Model):
 
     ready = models.BooleanField(default=False)
     tenant_name = models.CharField(max_length=63, unique=True, db_index=True)
-    account_id = models.CharField(max_length=36, default=None, null=True)
-    org_id = models.CharField(max_length=36, default=None, null=True)
+    account_id = models.CharField(max_length=36, unique=True, default=None, null=True)
+    org_id = models.CharField(max_length=36, unique=True, default=None, null=True)
 
     def __str__(self):
         """Get string representation of Tenant."""
