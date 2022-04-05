984f472405733c4696c94c8f9aa2ced0cbd11c0a
Matt Holder <maholder@redhat.com>
2022-04-05 08:33:00 -0400

    linting

--- rbac/api/utils.py
+++ rbac/api/utils.py
@@ -1,11 +1,13 @@
 """Helper utilities for api module."""
+
 from django.db import transaction
+from management.utils import account_id_for_tenant
 
 from api.models import Tenant
-from management.utils import account_id_for_tenant
 
 
 def populate_tenant_account_id():
+    """Populate tenant's account id's."""
     tenants = Tenant.objects.filter(account_id__isnull=True).exclude(tenant_name="public")
     for tenant in tenants:
         with transaction.atomic():
