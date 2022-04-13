cf3812953ec25a956b216990ae26370c9b350d3e
Matt Holder <maholder@redhat.com>
2022-04-13 11:00:05 -0400

    fix EOF error

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -380,4 +380,4 @@ AUTHENTICATE_WITH_ORG_ID = ENVIRONMENT.bool("AUTHENTICATE_WITH_ORG_ID", default=
 
 CHANNEL_LAYERS = {
     "default": {"BACKEND": "channels_redis.core.RedisChannelLayer", "CONFIG": {"hosts": [(REDIS_HOST, REDIS_PORT)]}}
-
+}
