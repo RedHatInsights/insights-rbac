9af3c92e937aa00623416085615c30def99c9bcf
Chris Mitchell <cmitchel@redhat.com>
2022-05-17 10:02:38 -0400

    Configure containers to log with watchtower
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- deploy/init-container-setup.sh
+++ deploy/init-container-setup.sh
@@ -5,6 +5,6 @@ export ACCESS_CACHE_CONNECT_SIGNALS=False
 echo "Starting init container script."
 
 echo "Running schema migrations <----"
-python /opt/app-root/src/rbac/manage.py migrate --noinput
+python /opt/app-root/src/rbac/manage.py migrate --noinput --settings=rbac.settings
 echo "Running seeds <-------"
-python /opt/app-root/src/rbac/manage.py seeds
+python /opt/app-root/src/rbac/manage.py seeds --settings=rbac.settings
