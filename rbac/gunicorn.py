f3f9f22e2604745718b03126ada2176c9f6682d6
Keith Walsh <kwalsh@redhat.com>
2022-07-19 20:14:43 -0400

    Parameterize gunicorn worker count multiplier
    A follow-up to #733 where we bumped the multiplier from 2 to 4. This defaults
    to 2 (as it was) and will allow us to override the param in production to bump
    as needed.

--- rbac/gunicorn.py
+++ rbac/gunicorn.py
@@ -4,6 +4,6 @@ import os
 
 bind = "unix:/var/run/rbac/gunicorn.sock"
 cpu_resources = int(os.environ.get("POD_CPU_LIMIT", multiprocessing.cpu_count()))
-workers = cpu_resources * 4
+workers = cpu_resources * int(os.environ.get("SERVER_WORKER_MULTIPLIER", 2))
 threads = 10
 limit_request_field_size = 16380
