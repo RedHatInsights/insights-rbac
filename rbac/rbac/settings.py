45b2682b63cbdea6234d8e1facc076ddda8e0166
Matt Holder <mjholder96@gmail.com>
2022-10-03 12:59:17 -0400

    removed accidental import

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -25,7 +25,6 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-from ast import And
 import os
 
 import datetime
