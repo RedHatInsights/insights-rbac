0c5fce4902667639894bc51d0a71126b66ec618b
Libor Pichler <lpichler@redhat.com>
2023-01-13 15:37:47 +0100

    Policy tho1

--- rbac/management/policy/model.py
+++ rbac/management/policy/model.py
@@ -179,8 +179,3 @@ if settings.ACCESS_CACHE_ENABLED and settings.ACCESS_CACHE_CONNECT_SIGNALS:
     signals.post_save.connect(policy_to_roles_sync_handler, sender=Policy)
     signals.pre_delete.connect(policy_changed_cache_handler, sender=Policy)
     signals.m2m_changed.connect(policy_to_roles_cache_handler, sender=Policy.roles.through)
-
-if settings.KAFKA_ENABLED:
-    #signals.post_save.connect(policy_changed_sync_handler, sender=Policy)
-    #signals.pre_delete.connect(policy_changed_sync_handler, sender=Policy)
-    #signals.m2m_changed.connect(policy_to_roles_sync_handler, sender=Policy.roles.through)
