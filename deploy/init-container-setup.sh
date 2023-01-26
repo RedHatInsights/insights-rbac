bd5f0a2ccdc6c991e6cbb0075517710234698da3
Ashley Aiken <aaiken@redhat.com>
2023-01-26 13:51:43 -0500

    add SHOULD_MIGRATE env var

--- deploy/init-container-setup.sh
+++ deploy/init-container-setup.sh
@@ -3,8 +3,14 @@
 export ACCESS_CACHE_CONNECT_SIGNALS=False
 
 echo "Starting init container script."
+MIGRATE=$(echo "$SHOULD_MIGRATE" | tr '[:upper:]' '[:lower:]')
 
-echo "Running schema migrations <----"
-python /opt/rbac/rbac/manage.py migrate --noinput
-echo "Running seeds <-------"
-python /opt/rbac/rbac/manage.py seeds
+if [[ "$MIGRATE" = "true" ]]
+then
+    echo "Running schema migrations <----"
+    python /opt/rbac/rbac/manage.py migrate --noinput
+    echo "Running seeds <-------"
+    python /opt/rbac/rbac/manage.py seeds
+else
+    echo "Migrations should not be ran <----"
+fi
