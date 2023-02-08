07a84a0f525583ad8905301f3ac0f1e3989e13ce
Ashley Aiken <aaiken@redhat.com>
2023-02-08 13:24:38 -0500

    [RHCLOUD-23491] Update Django to 3.2 (#795)
    * Update to Django 3.2.16

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -188,6 +188,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
