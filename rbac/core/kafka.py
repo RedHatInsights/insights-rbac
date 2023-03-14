e513c0cc33b45f64b9cc296e4a892202f98cbd35
Libor Pichler <lpichler@redhat.com>
2023-03-14 11:32:23 +0100

    WIP

--- rbac/core/kafka.py
+++ rbac/core/kafka.py
@@ -17,9 +17,8 @@
 """Producer to send messages to kafka server."""
 import json
 
+from confluent_kafka import Producer
 from django.conf import settings
-from kafka import KafkaProducer
-
 
 class FakeKafkaProducer:
     """Fake kafaka producer to enable local development without kafka server."""
@@ -28,20 +27,49 @@ class FakeKafkaProducer:
         """No operation method."""
         pass
 
+    def produce(self, topic, key=None, value=None, headers=None):
+        """No operation method."""
+        pass
+
 
 class RBACProducer:
     """Kafka message producer to emit events to notification service."""
-
     def get_producer(self):
+        with open('filename.txt', 'a') as f:
+            f.write('get_producer\n')
         """Init method to return fake kafka when flag is set to false."""
         if not hasattr(self, "producer"):
+            with open('filename.txt', 'a') as f:
+                f.write('producer\n')
             if settings.DEVELOPMENT or settings.MOCK_KAFKA or not settings.KAFKA_ENABLED:
+                with open('filename.txt', 'a') as f:
+                    f.write('fake\n')
+
                 self.producer = FakeKafkaProducer()
             else:
+                with open('filename.txt', 'a') as f:
+                    f.write('prod\n')
+
                 if settings.KAFKA_AUTH:
-                    self.producer = KafkaProducer(**settings.KAFKA_AUTH)
+                    with open('filename.txt', 'a') as f:
+                        f.write('KAFKA_AUTH\n')
+
+                    conf = {
+                        "bootstrap.servers": settings.KAFKA_AUTH["bootstrap_servers"],
+                        "security.protocol": settings.KAFKA_AUTH["security_protocol"],
+                        "sasl.mechanism": settings.KAFKA_AUTH["sasl_mechanism"],
+                        "sasl.username": settings.KAFKA_AUTH["sasl_plain_username"],
+                        "sasl.password": settings.KAFKA_AUTH["sasl_plain_password"],
+                    }
+
+                    self.producer = Producer(conf)
                 else:
-                    self.producer = KafkaProducer(bootstrap_servers=settings.KAFKA_SERVER)
+                    with open('filename.txt', 'a') as f:
+                        f.write('NOT AUTH\n')
+                    conf = {"bootstrap.servers": settings.KAFKA_SERVER}
+
+                    self.producer = Producer(conf)
+
         return self.producer
 
     def send_kafka_message(self, topic, message, headers=None):
@@ -50,7 +78,7 @@ class RBACProducer:
         json_data = json.dumps(message).encode("utf-8")
         if headers and not isinstance(headers, list):
             headers = [headers]
-        producer.send(topic, value=json_data, headers=headers)
+        producer.produce(topic, key=None, value=json_data, headers=headers)
 
 
 """
