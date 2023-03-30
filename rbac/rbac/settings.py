b113644da4856334f396c20d75a8f563d6294788
Ashley Aiken <aaiken@redhat.com>
2023-03-30 14:54:58 -0400

    [RHCLOUD-23259] Update sentry & SENTRY_DSN to use GLITCHTIP_DSN (#843)
    * Update SENTRY_DSN to use GLITCHTIP_DSN

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -50,16 +50,20 @@ from .env import ENVIRONMENT
 # Sentry monitoring configuration
 # Note: Sentry is disabled unless it is explicitly turned on by setting DSN
 
+GLITCHTIP_DSN = os.getenv("GLITCHTIP_DSN", "")
 SENTRY_DSN = os.getenv("SENTRY_DSN", "")
-if SENTRY_DSN:
+DSN = GLITCHTIP_DSN or SENTRY_DSN
+if DSN:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
 
-    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration(), RedisIntegration()])
+    sentry_sdk.init(dsn=DSN, integrations=[DjangoIntegration(), RedisIntegration()])
+    if GLITCHTIP_DSN:
+        print("Sentry SDK connected to Glitchtip.")
     print("Sentry SDK initialization was successful!")
 else:
-    print("SENTRY_DSN was not set, skipping Sentry initialization.")
+    print("GLITCHTIP_DSN was not set, skipping Sentry initialization.")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
