a4debb72f361c386e3df016fed31f2196d3bd38f
Ashley Aiken <aaiken@redhat.com>
2023-04-04 16:01:28 -0400

    Remove sentry fallback (#845)
    * remove sentry fallback
    
    * Make sure log messages are clear that we are using glitchtip dsn

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -49,21 +49,18 @@ from .env import ENVIRONMENT
 
 # Sentry monitoring configuration
 # Note: Sentry is disabled unless it is explicitly turned on by setting DSN
+# Note: Although we are using sentry-sdk we are connecting to Glitchtip DSN
 
 GLITCHTIP_DSN = os.getenv("GLITCHTIP_DSN", "")
-SENTRY_DSN = os.getenv("SENTRY_DSN", "")
-DSN = GLITCHTIP_DSN or SENTRY_DSN
-if DSN:
+if GLITCHTIP_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
 
-    sentry_sdk.init(dsn=DSN, integrations=[DjangoIntegration(), RedisIntegration()])
-    if GLITCHTIP_DSN:
-        print("Sentry SDK connected to Glitchtip.")
-    print("Sentry SDK initialization was successful!")
+    sentry_sdk.init(dsn=GLITCHTIP_DSN, integrations=[DjangoIntegration(), RedisIntegration()])
+    print("Sentry SDK initialization using Glitchtip was successful!")
 else:
-    print("GLITCHTIP_DSN was not set, skipping Sentry initialization.")
+    print("GLITCHTIP_DSN was not set, skipping Glitchtip initialization.")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
