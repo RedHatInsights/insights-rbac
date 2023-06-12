7b2e1224ae3e681e84a5df24910072ff938f5e95
Libor Pichler <lpichler@redhat.com>
2023-06-12 16:56:00 +0200

    Change a way for principal membership in groups

--- rbac/management/group/view.py
+++ rbac/management/group/view.py
@@ -22,6 +22,7 @@ from django.conf import settings
 from django.db.models.aggregates import Count
 from django.utils.translation import gettext as _
 from django_filters import rest_framework as filters
+from django.db import transaction
 from management.filters import CommonFilters
 from management.group.definer import add_roles, remove_roles, set_system_flag_before_update
 from management.group.model import Group
@@ -377,12 +378,17 @@ class GroupViewSet(
 
     def remove_principals(self, group, principals, account=None, org_id=None):
         """Process list of principals and remove them from the group."""
+
+        req_id = getattr(self.request, "req_id", None)
+        logger.info(f"[Request_id:{req_id}] remove_principals for group {group.name} for org id {org_id} started...")
+
         if settings.AUTHENTICATE_WITH_ORG_ID:
             tenant = Tenant.objects.get(org_id=org_id)
         else:
             tenant = Tenant.objects.get(tenant_name=f"acct{account}")
 
-        valid_usernames = Principal.objects.filter(group=group, tenant=tenant, username__in=principals).values_list(
+        valid_principals = Principal.objects.filter(group=group, tenant=tenant, username__in=principals)
+        valid_usernames = valid_principals.values_list(
             "username", flat=True
         )
         usernames_diff = set(principals) - set(valid_usernames)
@@ -402,8 +408,11 @@ class GroupViewSet(
                 ],
             }
 
-        group.principals.filter(username__in=valid_usernames).delete()
-        logger.info(f"Principals {valid_usernames} removed from group {group.name} for org id {org_id}.")
+        with transaction.atomic():
+            for principal in valid_principals:
+                group.principals.remove(principal)
+
+        logger.info(f"[Request_id:{req_id}] Principals {valid_usernames} removed from group {group.name} for org id {org_id}.")
         for username in principals:
             group_principal_change_notification_handler(self.request.user, group, username, "removed")
         return group
