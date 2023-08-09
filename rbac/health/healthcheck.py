d76ea4b6aaa1662b626749375f8ae0aa0e15cfc1
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2023-08-09 13:55:40 -0700

    Trying to save these changes and trying to change rbac clowd app related matters

---
+++ rbac/health/healthcheck.py
@@ -0,0 +1,36 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Celery Health Check."""
+from time import sleep
+
+import health_check
+
+rbacHealthCheck = health_check.contrib.celery_ping.backends.CeleryPingHealthCheck()
+
+
+def delay():
+    """Delay 10 seconds for async."""
+    sleep(10)
+
+
+def check_health():
+    """Check the health of the workers and brokers."""
+    # If the celery workers or redis connection are unavailable, then return a 500 error.
+    # Otherwise, return a 200 status."""
+
+    check_status = rbacHealthCheck.check_status()
+    return check_status
