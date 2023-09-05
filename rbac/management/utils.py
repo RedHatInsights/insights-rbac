2cba7e4af84b5ef23c56f3335bb7d33dd8f6dd04
Shannon Donahue <sdonahue@redhat.com>
2023-09-05 09:47:04 -0400

    Fix options parameter passing.

--- rbac/management/utils.py
+++ rbac/management/utils.py
@@ -89,13 +89,12 @@ def verify_principal_with_proxy(username, request, verify_principal=True):
     """Verify username through the BOP."""
     account = request.user.account
     org_id = request.user.org_id
-    query_options={"status": request.query_params.get("status")}
     proxy = PrincipalProxy()
     if verify_principal:
         if settings.AUTHENTICATE_WITH_ORG_ID:
-            resp = proxy.request_filtered_principals([username], org_id=org_id, options = query_options)
+            resp = proxy.request_filtered_principals([username], org_id=org_id, options = request.query_params)
         else:
-            resp = proxy.request_filtered_principals([username], account, options = query_options)
+            resp = proxy.request_filtered_principals([username], account, options = request.query_params)
 
         if isinstance(resp, dict) and "errors" in resp:
             raise Exception("Dependency error: request to get users from dependent service failed.")
