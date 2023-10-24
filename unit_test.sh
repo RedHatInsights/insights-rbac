6d23dfb0d55d83718ab3322af2314d5ba9c6f041
Daniel Agbay <Daniel Agbay>
2023-10-24 14:59:22 -0500

    added logs

--- unit_test.sh
+++ unit_test.sh
@@ -4,11 +4,15 @@ source $CICD_ROOT/deploy_ephemeral_db.sh
 # Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
 export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
 
+echo "Running rbac unit tests in virtual env..."
+
 pip3 install pipenv
 pipenv install --dev
 pipenv shell
 tox -r
 result=$?
+
+echo "RBAC tests finished running..."
 
 source .bonfire_venv/bin/activate
 
