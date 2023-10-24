4f4eccd04e563b847f484b31783f985744a5f760
Daniel Agbay <Daniel Agbay>
2023-10-24 14:47:40 -0500

    test using pipenv

--- unit_test.sh
+++ unit_test.sh
@@ -4,11 +4,12 @@ source $CICD_ROOT/deploy_ephemeral_db.sh
 # Map env vars set by `deploy_ephemeral_db.sh` if vars the app uses are different
 export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
 
-python3 -m venv app-venv
-source app-venv/bin/activate
-pip install --upgrade pip setuptools wheel pipenv tox psycopg2-binary
+pip3 install pipenv
+pipenv install --dev
+pipenv shell
 tox -r
 result=$?
+
 source .bonfire_venv/bin/activate
 
 # TODO: add unittest-xml-reporting to rbac so that junit results can be parsed by jenkins
