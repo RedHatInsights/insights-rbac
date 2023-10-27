b102671072628a8d88fdf0907f6dff7e2225514d
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2023-10-26 19:05:10 -0700

    Seeing whether we can try to add the dependencies through a virtual env wihtin bonfire

--- unit_test.sh
+++ unit_test.sh
@@ -1,9 +1,14 @@
-#what we need to do here is try to connect to the ephemeral env that we created
-#so that we can run the unit tests and the smoke tests 
+#create my own new ephemeral database
+#and then we can try to connect to it 
+# and try to run the unit tets
 
-docker logs 
+source $CICD_ROOT/deploy_ephemeral_db.sh
 
-printenv 
+export PGPASSWORD=$DATABASE_ADMIN_PASSWORD
+
+source .bonfire_venv/bin/activate
+sudo yum install python3.9 
+sudo yum install libpq-devel 
 
 python3 --version 
 python3.9 -m venv app-venv
