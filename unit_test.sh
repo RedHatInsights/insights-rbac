89b090fce3d3613c763564e04006ca3774cd4766
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2023-11-06 14:18:07 -0800

    trying to run rbac-test

--- unit_test.sh
+++ unit_test.sh
@@ -26,7 +26,7 @@ set -ex
 # fi
 
 # Start up db container defined in docker-compose file in order to try to connect to postgresql 
-docker-compose run -f ./rbac-test.yml --detach
+docker compose run -f ./rbac-test.yml --detach
 
 # Build PR_CHECK Image
 docker build -f './Dockerfile-pr-check' --label $CONTAINER_NAME --tag $IMAGE_TAG .
