70fc897635215d012e68c6b5fcf6e39b89bf0993
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2023-11-15 12:45:22 -0800

    Trying to see whether we can list all of the pods and remove the new image tag?

--- unit_test.sh
+++ unit_test.sh
@@ -48,9 +48,13 @@ echo "Killing DB Container..."
 docker kill $DB_CONTAINER
 echo "Removing DB Container..."
 docker rm -f $DB_CONTAINER
-echo "Killing pr check container" 
+
+
+docker ps -a
+
+echo "Killing pr check container..." 
 docker kill $CONTAINER_NAME
-echo "Removing pr check container" 
+echo "Removing pr check container..." 
 docker rm -f $CONTAINER_NAME
 
 if [[ $OUT_CODE != 0 ]]; then
