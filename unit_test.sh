267798969346f75a3bd7aaf67b84767c529fa042
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2023-11-15 10:38:48 -0800

    creating the database container and trying to run the tox env

--- unit_test.sh
+++ unit_test.sh
@@ -26,16 +26,41 @@ set -ex
 # fi
 
 # Start up db container defined in docker-compose file in order to try to connect to postgresql 
-#docker compose -f ./rbac-test.yml run --detach
-docker network create rbac-network
-make start-db
-make run-migrations
+# spin up the db for integration tests
+DB_CONTAINER="rbac-$(uuidgen)"
+echo "Spinning up container: ${DB_CONTAINER}"
+
+docker run -d \
+    --name $DB_CONTAINER \
+    -p 5432 \
+    -e POSTGRESQL_USER=postgres \
+    -e POSTGRESQL_PASSWORD=postgres \
+    -e POSTGRESQL_DATABASE=rbac \
+    quay.io/cloudservices/postgresql-rds:14-1
+
+PORT=$(docker inspect $DB_CONTAINER | grep HostPort | sort | uniq | grep -o [0-9]*)
+echo "DB Listening on Port: ${PORT}"
+
+export DATABASE_HOST=localhost
+export DATABASE_PORT=$PORT
+export DATABASE_USER=postgres
+export DATABASE_PASSWORD=postgres
+export DATABASE_NAME=rbac
+
+echo "Running tests...here we go"
+
 
 # Build PR_CHECK Image
 docker build -f './Dockerfile-pr-check' --label $CONTAINER_NAME --tag $IMAGE_TAG .
 
 # Build PR_Check Container
-docker create --name $CONTAINER_NAME $IMAGE_TAG tox
+docker create --name $CONTAINER_NAME \
+    -e DATABASE_NAME=$DATABASE_NAME \
+    -e DATABASE_HOST=$DATABASE_HOST \
+    -e DATABASE_PORT=$DATABASE_PORT \
+    -e DATABASE_USERNAME=$DATABASE_USERNAME \
+    -e DATABASE_PASSWORD=$DATABASE_PASSWORD \
+    $IMAGE_TAG tox
 
 # Run PR_CHECK Container (attached with standard output)
 # and reports if the Containerized PR_Check fails
@@ -44,6 +69,15 @@ if ! (docker start -a $CONTAINER_NAME); then
     exit 1
 fi
 
+echo "Killing DB Container..."
+docker kill $DB_CONTAINER
+echo "Removing DB Container..."
+docker rm -f $DB_CONTAINER
+
+
+if [[ $OUT_CODE != 0 ]]; then
+    exit 1
+fi
 # Pass Jenkins dummy artifacts as it needs
 # an xml output to consider the job a success.
 mkdir -p $WORKSPACE/artifacts
