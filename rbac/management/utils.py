4312779ce6f25d1cfd6d2dcd1fe602161340489d
Mikel Alejo <mbarcina@redhat.com>
2023-11-27 16:59:09 +0100

    RHCLOUD-29427 | fix: service account usernames are being incorrectly saved (#970)
    * update username for service accounts
    
    * update the username to match IT response
    
    * fix: service accounts have mismatching names
    
    The issue comes from two places:
    
    1) Whenever a service account hit the "/access" endpoint, we didn't have
       a proper way to store the service account information in the
       "principals" table. Basically we would store the service account's
       username along with a "user" type, to begin with. This was due to the
       "get or create" function not being able to handle the service
       account's client ID and its type.
    2) Upon creating a service account through the
       "/groups/{uuid}/principals/" endpoint, the usernames we were given
       from the UI were directly taken from the IT service accounts' names,
       which are essentially some kind of "display name", as confirmed by
       them.
    
    These two issues combined made the users who sent requests to RBAC with
    service accounts to not get any results back, because the usernames
    would never match.
    
    RHCLOUD-29427
    
    * refactor: apply review feedback
    
    ---------
    
    Co-authored-by: Ashley Aiken <aaiken@redhat.com>

--- rbac/management/utils.py
+++ rbac/management/utils.py
@@ -27,12 +27,13 @@ from management.permissions.principal_access import PrincipalAccessPermission
 from management.principal.proxy import PrincipalProxy
 from rest_framework import serializers, status
 
-from api.models import CrossAccountRequest, Tenant
+from api.models import CrossAccountRequest, Tenant, User
 
 
 USERNAME_KEY = "username"
 APPLICATION_KEY = "application"
 PRINCIPAL_PERMISSION_INSTANCE = PrincipalAccessPermission()
+SERVICE_ACCOUNT_KEY = "service-account"
 
 
 def validate_psk(psk, client_id):
@@ -76,6 +77,13 @@ def get_principal(username, request, verify_principal=True, from_query=False):
         principal = Principal.objects.get(username__iexact=username, tenant=tenant)
     except Principal.DoesNotExist:
         verify_principal_with_proxy(username, request, verify_principal=verify_principal)
+
+        # In the case that the user that made the request was a service account, store it accordingly in the database.
+        user: User = request.user
+        if user and user.is_service_account:
+            principal = Principal.objects.get_or_create(
+                username=user.username, tenant=tenant, type=SERVICE_ACCOUNT_KEY, service_account_id=user.client_id
+            )
 
         # Avoid possible race condition if the user was created while checking BOP
         principal, created = Principal.objects.get_or_create(
