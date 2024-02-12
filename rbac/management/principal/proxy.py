be2dc8daf4c32f753d954ec293a6eef2ac0dea67
Ashley Aiken <aaiken@redhat.com>
2024-02-12 09:21:18 -0500

    Fix log statement for proxy tests

--- rbac/management/principal/proxy.py
+++ rbac/management/principal/proxy.py
@@ -211,13 +211,6 @@ class PrincipalProxy:  # pylint: disable=too-few-public-methods
             if self.source_cert:
                 kwargs["verify"] = self.client_cert_path
             response = method(url, **kwargs)
-            LOGGER.info(
-                "Response directly from BOP for org id %s and principal %s. Status: %s, Response: %s",
-                org_id,
-                data,
-                str(response.status_code),
-                str(response.json()),
-            )
         except requests.exceptions.ConnectionError as conn:
             LOGGER.error("Unable to connect for URL %s with error: %s", url, conn)
             resp = {"status_code": status.HTTP_500_INTERNAL_SERVER_ERROR, "errors": [unexpected_error]}
@@ -230,7 +223,17 @@ class PrincipalProxy:  # pylint: disable=too-few-public-methods
             """Testing if account numbers match"""
             if settings.AUTHENTICATE_WITH_ORG_ID:
                 try:
+                    principal_list = []
+                    if data:
+                        principal_list = data.get("users")
                     data = response.json()
+                    LOGGER.info(
+                        "Response directly from BOP for org id %s and principal %s. Status: %s, Response: %s",
+                        org_id,
+                        str(principal_list),
+                        str(response.status_code),
+                        str(data),
+                    )
                     if isinstance(data, dict):
                         userList = self._process_data(data.get("users"), org_id, org_id_filter, return_id)
                         resp["data"] = {"userCount": data.get("userCount"), "users": userList}
