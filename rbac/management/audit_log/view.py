5379cf456c37585d2cab6248a643df2fb7e14b9c
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2024-03-07 15:34:11 -0800

    Update view.py according to comments and include filter by tenant_id
    Add audit log to serializers, urls, and views

--- rbac/management/audit_log/view.py
+++ rbac/management/audit_log/view.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2023 Red Hat, Inc.
+# Copyright 2024 Red Hat, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,7 @@
 
 """View for Audit Logs."""
 from management.models import AuditLog
+from management.querysets import get_auditlog_queryset
 from management.serializers import AuditLogSerializer
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import AllowAny
@@ -26,13 +27,18 @@ class AuditLogViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """Audit Logs View.
 
     A viewset that provides default `list()` actions.
-
     """
 
     queryset = AuditLog.objects.all()
     serializer_class = AuditLogSerializer
     permission_classes = (AllowAny,)
 
-    def get(self, request, *args, **kwargs):
-        """List all of the audiit logs that are stored within database."""
+    def get_queryset_by_tenant(self):
+        """Obtain queryset related to request tenant and verify that user is admin."""
+        new_queryset = get_auditlog_queryset(self.request)
+        return new_queryset
+
+    def list(self, request, *args, **kwargs):
+        """List all of the audit logs within database by tenant."""
+        self.queryset = self.get_queryset_by_tenant()
         return super().list(request=request, args=args, kwargs=kwargs)
