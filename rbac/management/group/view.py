78c7197a9a71d071b5a6c1e34fd254e2fca6123c
Petra Číhalová <pcihalov@redhat.com>
2024-03-11 13:22:57 +0100

    the non org admins cannot remove principal from group with User Access Administrator role

--- rbac/management/group/view.py
+++ rbac/management/group/view.py
@@ -833,6 +833,18 @@ class GroupViewSet(
         else:
             self.protect_system_groups("remove principals")
 
+            # Only organization administrators are allowed to remove principals from groups with the
+            # "User Access Administrator" role.
+            if not request.user.admin:
+                for role in group.roles_with_access():
+                    if role.name == USER_ACCESS_ADMINISTRATOR_ROLE_KEY:
+                        key = "remove_principals"
+                        message = (
+                            "Non-admin users may not remove principals from Groups with "
+                            f"'{USER_ACCESS_ADMINISTRATOR_ROLE_KEY}' role."
+                        )
+                        raise serializers.ValidationError({key: _(message)})
+
             if SERVICE_ACCOUNTS_KEY not in request.query_params and USERNAMES_KEY not in request.query_params:
                 key = "detail"
                 message = "Query parameter {} or {} is required.".format(SERVICE_ACCOUNTS_KEY, USERNAMES_KEY)
