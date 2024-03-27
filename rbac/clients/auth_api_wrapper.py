de6c3165768f4d3e928920ec9c13159cf06fe49f
Libor Pichler <lpichler@redhat.com>
2024-03-27 13:15:52 +0100

    wrap grpc call into class

---
+++ rbac/clients/auth_api_wrapper.py
@@ -0,0 +1,44 @@
+import grpc
+from clients.relation_api_grpc import relationships_pb2
+from clients.relation_api_grpc import relationships_pb2_grpc
+from rbac.env import ENVIRONMENT
+
+
+class AuthAPIWrapper:
+    def __init__(self):
+        self.grpc_server_address = ENVIRONMENT.get_value("RELATION_API_HOST", default="") + ":" + ENVIRONMENT.get_value("RELATION_API_PORT", default="")
+
+    def test_grpc_call(self):
+        with grpc.insecure_channel(self.grpc_server_address) as channel:
+            stub = relationships_pb2_grpc.RelationshipsStub(channel)
+
+            request = relationships_pb2.CreateRelationshipsRequest(
+                touch=True,
+                relationships=[
+                    relationships_pb2.Relationship(
+                        object=relationships_pb2.ObjectReference(type="group", id="bob_club"),
+                        relation="member",
+                        subject=relationships_pb2.SubjectReference(
+                            object=relationships_pb2.ObjectReference(type="user", id="bob")
+                        )
+                    )
+                ]
+            )
+
+            response = stub.CreateRelationships(request)
+        print(response.SerializeToString())
+        print("End of CreateRelationshipsRequest")
+
+        print("Start of ReadRelationshipsRequest")
+        with grpc.insecure_channel(self.grpc_server_address) as channel:
+            stub = relationships_pb2_grpc.RelationshipsStub(channel)
+
+            request = relationships_pb2.ReadRelationshipsRequest(filter=relationships_pb2.RelationshipFilter(
+                    object_type="group",
+                    object_id="bob_club",
+                    relation="member"
+                ))
+
+            response = stub.ReadRelationships(request)
+        print(response)
+        print("End of ReadRelationshipsRequest")
