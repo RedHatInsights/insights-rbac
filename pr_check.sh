7859f6855894aad8fa469688c016ae9c13b64d6c
Matt Holder <mjholder96@gmail.com>
2024-04-22 09:53:12 -0400

    shouldnt need these anymore

--- pr_check.sh
+++ pr_check.sh
@@ -14,9 +14,7 @@ export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
 export IQE_TEST_IMPORTANCE="critical" # This is the value passed to iqe --testImportance
 export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
 
-export BONFIRE_REPO_ORG="Victoremepunto"
 export CICD_REPO_ORG="Victoremepunto"
-export BONFIRE_REPO_BRANCH="refactor-bootstrap-paths"
 export CICD_REPO_BRANCH="refactor-bootstrap-paths"
 
 # Install bonfire repo/initialize
