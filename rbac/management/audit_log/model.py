c4747334a6042fc7489e257aa0b96f630f86ac5a
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2024-05-21 14:29:04 -0700

    Update PR with changes mentioned

--- rbac/management/audit_log/model.py
+++ rbac/management/audit_log/model.py
@@ -64,51 +64,48 @@ class AuditLog(TenantAwareModel):
     tenant = models.ForeignKey(Tenant, on_delete=models.SET_NULL, null=True)
 
     def get_resource_item(self, r_type, request, *args, **kwargs):
-        """Find different values of various fields."""
+        """Find related items (eg, name, id, etc...) to each resource time."""
         if r_type == AuditLog.ROLE:
             role_items = []
             if request.data != {}:
                 role_object = get_object_or_404(Role, name=request.data["name"])
-                role_object_id = role_object.id
-                role_object_name = "role: " + role_object.name
-                role_items.append(role_object_id)
-                role_items.append(role_object_name)
-                return role_items
             else:
                 role_object = kwargs["kwargs"]
-                role_object_id = role_object.id
-                role_object_name = "role: " + role_object.name
-                role_items.append(role_object_id)
-                role_items.append(role_object_name)
-                return role_items
+            # retrieve role id and name
+            role_object_id = role_object.id
+            role_object_name = "role: " + role_object.name
+            role_items.append(role_object_id)
+            role_items.append(role_object_name)
+            return role_items
 
         elif r_type == AuditLog.GROUP:
+            group_items = []
             if request._data is not None:
                 group_object = get_object_or_404(Group, name=request.data["name"])
             else:
                 group_uuid = kwargs["kwargs"]["uuid"]
                 group_object = get_object_or_404(Group, uuid=group_uuid)
-            return group_object.id
+                group_object_id = group_object.id
+            group_object_name = "group: " + group_object.name
+            group_items.append(group_object_id)
+            group_items.append(group_object_name)
+            return group_items
 
         elif r_type == AuditLog.PERMISSION:
-            # TODO: finding the id for the permission
+            # TODO: update for permission related items
             return None
 
         elif r_type == "principal":
             current_user = management.utils.get_principal_from_request(request)
             principal_object = get_object_or_404(Principal, username=current_user.username)
-            list_principal_object = [principal_object.id, principal_object.username]
-            return list_principal_object
+            return [principal_object.id, principal_object.username]
 
         elif r_type == "tenant":
             tenant_object = get_object_or_404(Tenant, org_id=request._user.org_id)
             return tenant_object.id
 
-        else:
-            return ValueError("This resource type does not exist or is not role/group/tenant/permission/principal")
-
     def log_create(self, request, resource):
-        """Audit Log when group or user is created."""
+        """Audit Log when a role or a group is created."""
         principal_items = self.get_resource_item("principal", request)
         self.principal_id = principal_items[0]
         self.principal_username = principal_items[1]
@@ -116,6 +113,7 @@ class AuditLog(TenantAwareModel):
         create_resource_items = self.get_resource_item(resource, request)
 
         self.resource_type = resource
+
         self.resource_id = create_resource_items[0]
         self.description = "Created " + create_resource_items[1]
 
