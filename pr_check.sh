c4a794921a38813834303bba5b8cd8a6d5dab06a
Brandon Squizzato <bsquizza@redhat.com>
2024-06-12 16:35:45 -0400

    Test adding grafana URL via cicd-tools

--- pr_check.sh
+++ pr_check.sh
@@ -18,6 +18,7 @@ export IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to compl
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+CICD_REPO_BRANCH=print_grafana_url
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 # Build the image and push to quay
