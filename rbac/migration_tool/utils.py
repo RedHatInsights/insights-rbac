f0c3fb044452b1249a8abc700df47c9f5d9959f5
Keith Walsh <kwalsh@redhat.com>
2024-08-14 14:34:54 -0400

    Remove superfluous error handler and update calls
    This updates the error logging/raising for gRPC calls by accessing the `code()` and
    `details()` methods on `grpc.RpcError` directly.

--- rbac/migration_tool/utils.py
+++ rbac/migration_tool/utils.py
@@ -1,11 +1,8 @@
 """Utilities for working with the relation API server."""
-import json
 import logging
 
 import grpc
 from django.conf import settings
-from google.rpc import error_details_pb2
-from grpc_status import rpc_status
 from kessel.relations.v1beta1 import common_pb2
 from kessel.relations.v1beta1 import relation_tuples_pb2
 from kessel.relations.v1beta1 import relation_tuples_pb2_grpc
@@ -13,28 +10,6 @@ from protoc_gen_validate.validator import ValidationFailed, validate_all
 
 
 logger = logging.getLogger(__name__)
-
-
-class GRPCError:
-    """A wrapper for a gRPC error."""
-
-    code: grpc.StatusCode
-    reason: str
-    message: str
-    metadata: dict
-
-    def __init__(self, error: grpc.RpcError):
-        """Initialize the error."""
-        self.code = error.code()
-        self.message = error.details()
-
-        status = rpc_status.from_call(error)
-        if status is not None:
-            detail = status.details[0]
-            info = error_details_pb2.ErrorInfo()
-            detail.Unpack(info)
-            self.reason = info.reason
-            self.metadata = json.loads(str(info.metadata).replace("'", '"'))
 
 
 def validate_and_create_obj_ref(obj_name, obj_id):
@@ -74,9 +49,8 @@ def write_relationships(relationships):
         try:
             stub.CreateTuples(request)
         except grpc.RpcError as err:
-            error = GRPCError(err.value)
             logger.error(
                 "Failed to write relationships to the relation API server: "
-                f"error code {error.code}, reason {error.reason}"
+                f"error code {err.code()}, reason {err.details()}"
                 f"relationships: {relationships}"
             )
