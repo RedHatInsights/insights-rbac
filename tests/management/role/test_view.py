4205ae3fa5c790fbc11ad03b467004da6b6c060d
Alec Henninger <alechenninger@gmail.com>
2024-09-03 15:55:18 -0400

    Fix tests that reference renamed method

--- tests/management/role/test_view.py
+++ tests/management/role/test_view.py
@@ -337,7 +337,7 @@ class RoleViewsetTests(IdentityRequest):
                 ANY,
             )
 
-    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler.save_replication_event")
+    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler._save_replication_event")
     def test_create_role_with_display_success(self, mock_method):
         """Test that we can create a role."""
         role_name = "roleD"
@@ -1365,7 +1365,7 @@ class RoleViewsetTests(IdentityRequest):
         response = client.put(url, test_data, format="json", **self.headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler.save_replication_event")
+    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler._save_replication_event")
     def test_update_role(self, mock_method):
         """Test that updating a role with an invalid permission returns an error."""
         # Set up
@@ -1488,7 +1488,7 @@ class RoleViewsetTests(IdentityRequest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data.get("errors")[0].get("detail"), f"Permission does not exist: {permission}")
 
-    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler.save_replication_event")
+    @patch("management.role.relation_api_dual_write_handler.RelationApiDualWriteHandler._save_replication_event")
     def test_delete_role(self, mock_method):
         """Test that we can delete an existing role."""
         role_name = "roleA"
