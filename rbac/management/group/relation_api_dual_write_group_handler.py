e0ba9a6b2ff07b357c377b7b78ab0c11bee51823
Libor Pichler <lpichler@redhat.com>
2024-09-16 12:24:51 +0200

    Add replication for group principal add/remove
    Extract principals validation in proxy request from add_principals in group view
    
    Extract service accounts validation from add_service_accounts in group view
    
    Allow to pass group into protect_system_groups in group view
    
    User different group object to  system group protection in group view
    
    Lock group and principals to add/remove principals in group view
    
    Generate replication event for adding principals into groups
    
    Update dual write handler after PR#1200 rebase
    
    Remove unnecesary query in relation_api_dual_write_group_handler and pass principal objects
    
    Decouple role from OutboxReplicator
    
    Add replication event test for adding principals
    
    Remove unnecessary token generation in removing principals from groups
    
    Replicate removed principals and service accounts from group into Relation API
    
    Extract user-group relations into method in relation api group handler
    
    Use constant for event type in group dual write handler
    
    Add more tests

---
+++ rbac/management/group/relation_api_dual_write_group_handler.py
@@ -0,0 +1,114 @@
+#
+# Copyright 2024 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Class to handle Dual Write API related operations."""
+import logging
+from typing import Optional
+
+from django.conf import settings
+from management.role.relation_api_dual_write_handler import (
+    DualWriteException,
+    OutboxReplicator,
+    RelationReplicator,
+    ReplicationEvent,
+    ReplicationEventType
+)
+from migration_tool.utils import create_relationship
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class RelationApiDualWriteGroupHandler:
+    """Class to handle Dual Write API related operations."""
+
+    def __init__(
+        self,
+        group,
+        event_type: ReplicationEventType,
+        principal_usernames,
+        service_accounts,
+        replicator: Optional[RelationReplicator] = None,
+    ):
+        """Initialize RelationApiDualWriteGroupHandler."""
+        if not self.replication_enabled():
+            return
+        try:
+            self.group_relations_to_add = []
+            self.group_relations_to_remove = []
+            self.principal_usernames = principal_usernames
+            self.service_accounts = service_accounts
+            self.group = group
+            self.event_type = event_type
+            self._replicator = replicator if replicator else OutboxReplicator(group)
+        except Exception as e:
+            raise DualWriteException(e)
+
+    def replication_enabled(self):
+        """Check whether replication enabled."""
+        return settings.REPLICATION_TO_RELATION_ENABLED is True
+
+    def _generate_relations(self):
+        """Generate user-groups relations."""
+        relations = []
+        for principal in self.principal_usernames + self.service_accounts:
+            relations.append(
+                create_relationship(
+                    ("rbac", "group"),
+                    str(self.group.uuid),
+                    ("rbac", "user"),
+                    str(principal.uuid),
+                    "member"
+                )
+            )
+
+        return relations
+
+    def replicate_new_principals(self):
+        """Replicate new principals into group."""
+        if not self.replication_enabled():
+            return
+        logger.info("[Dual Write] Generate new relations from Group(%s): '%s'", self.group.uuid, self.group.name)
+
+        self.group_relations_to_add = self._generate_relations()
+        self._replicate()
+
+    def replicate_removed_principals(self):
+        """Replicate removed principals from group."""
+        if not self.replication_enabled():
+            return
+        logger.info("[Dual Write] Generate new relations from Group(%s): '%s'", self.group.uuid, self.group.name)
+
+        self.group_relations_to_remove = self._generate_relations()
+
+        self._replicate()
+
+    def _replicate(self):
+        if not self.replication_enabled():
+            return
+        try:
+            self._replicator.replicate(
+                ReplicationEvent(
+                    type=self.event_type,
+                    # TODO: need to think about partitioning
+                    # Maybe resource id
+                    partition_key="rbactodo",
+                    remove=self.group_relations_to_remove,
+                    add=self.group_relations_to_add,
+                ),
+            )
+        except Exception as e:
+            raise DualWriteException(e)
