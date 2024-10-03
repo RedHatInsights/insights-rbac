1f795aeb732f3501d288568fe3d824699e584745
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2024-10-03 12:16:35 -0700

    adding paths to the code and testing for db server/trying curl command

--- konflux_unit_test.sh
+++ konflux_unit_test.sh
@@ -8,10 +8,16 @@ pip install pipenv
 pip install tox
 
 echo "testing for db server"
+ls
+cd
+ls -la
 ls /dev/
-DB_HOST=localhost DB_PORT=15432 bash -c 'printf "" 2>>/dev/null >>/dev/tcp/${DB_HOST}/${DB_PORT}'
+
+DB_HOST=localhost
+DB_PORT=15432
+printf "" 2>>/dev/null >>/dev/tcp/${DB_HOST}/${DB_PORT}
 
 echo $?
 echo "testing for db server--end"
-
+curl "http://localhost:15432"
 tox -r
