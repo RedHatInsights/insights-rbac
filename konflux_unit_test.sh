b9848a2246ee085944b3f4febcf5cac9a0ca830a
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2024-10-03 12:11:03 -0700

    echo message not appearing

--- konflux_unit_test.sh
+++ konflux_unit_test.sh
@@ -7,9 +7,11 @@ pip install --upgrade pip
 pip install pipenv
 pip install tox
 
+echo "testing for db server"
+ls /dev/
 DB_HOST=localhost DB_PORT=15432 bash -c 'printf "" 2>>/dev/null >>/dev/tcp/${DB_HOST}/${DB_PORT}'
 
-echo $? "testing for the db server"
-
+echo $?
+echo "testing for db server--end"
 
 tox -r
