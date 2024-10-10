7525f71ed4160878854b6474458b1a4e741558ca
wscalf <wscalf@gmail.com>
2024-10-10 14:56:33 -0400

    More seeding replicator

--- rbac/management/role/relation_api_dual_write_handler.py
+++ rbac/management/role/relation_api_dual_write_handler.py
@@ -40,6 +40,178 @@ from api.models import Tenant
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
+<<<<<<< HEAD
+=======
+class DualWriteException(Exception):
+    """DualWrite exception."""
+
+    pass
+
+
+class ReplicationEventType(str, Enum):
+    """Replication event type."""
+
+    CREATE_SYSTEM_ROLE = "create_system_role"
+    UPDATE_SYSTEM_ROLE = "update_system_role"
+    DELETE_SYSTEM_ROLE = "delete_system_role"
+    CREATE_CUSTOM_ROLE = "create_custom_role"
+    UPDATE_CUSTOM_ROLE = "update_custom_role"
+    DELETE_CUSTOM_ROLE = "delete_custom_role"
+    ASSIGN_ROLE = "assign_role"
+    UNASSIGN_ROLE = "unassign_role"
+    CREATE_GROUP = "create_group"
+    UPDATE_GROUP = "update_group"
+    DELETE_GROUP = "delete_group"
+    ADD_PRINCIPALS_TO_GROUP = "add_principals_to_group"
+    REMOVE_PRINCIPALS_FROM_GROUP = "remove_principals_from_group"
+    BOOTSTRAP_TENANT = "bootstrap_tenant"
+    EXTERNAL_USER_UPDATE = "external_user_update"
+
+
+class ReplicationEvent:
+    """What tuples changes to replicate."""
+
+    event_type: ReplicationEventType
+    event_info: dict[str, object]
+    partition_key: str
+    add: list[common_pb2.Relationship]
+    remove: list[common_pb2.Relationship]
+
+    def __init__(
+        self,
+        event_type: ReplicationEventType,
+        partition_key: str,
+        add: list[common_pb2.Relationship] = [],
+        remove: list[common_pb2.Relationship] = [],
+        info: dict[str, object] = {},
+    ):
+        """Initialize ReplicationEvent."""
+        self.partition_key = partition_key
+        self.event_type = event_type
+        self.add = add
+        self.remove = remove
+        self.event_info = info
+
+
+class RelationReplicator(ABC):
+    """Type responsible for replicating relations to Kessel Relations."""
+
+    @abstractmethod
+    def replicate(self, event: ReplicationEvent):
+        """Replicate the given event to Kessel Relations."""
+        pass
+
+
+class OutboxReplicator(RelationReplicator):
+    """Replicates relations via the outbox table."""
+
+    def replicate(self, event: ReplicationEvent):
+        """Replicate the given event to Kessel Relations via the Outbox."""
+        payload = self._build_replication_event(event.add, event.remove)
+        self._save_replication_event(payload, event.event_type, event.event_info, event.partition_key)
+
+    def _build_replication_event(self, relations_to_add, relations_to_remove):
+        """Build replication event."""
+        add_json = []
+        for relation in relations_to_add:
+            add_json.append(json_format.MessageToDict(relation))
+
+        remove_json = []
+        for relation in relations_to_remove:
+            remove_json.append(json_format.MessageToDict(relation))
+
+        replication_event = {"relations_to_add": add_json, "relations_to_remove": remove_json}
+        return replication_event
+
+    def _save_replication_event(self, payload, event_type, event_info: dict[str, object], aggregateid):
+        """Save replication event."""
+        # TODO: Can we add these as proper fields for kibana but also get logged in simple formatter?
+        logger.info(
+            "[Dual Write] Publishing replication event. event_type='%s' %s",
+            event_type,
+            " ".join([f"info.{key}='{str(value)}'" for key, value in event_info.items()]),
+        )
+        # https://debezium.io/documentation/reference/stable/transformations/outbox-event-router.html#basic-outbox-table
+        outbox_record = Outbox.objects.create(
+            aggregatetype="relations-replication-event",
+            aggregateid=aggregateid,
+            event_type=event_type,
+            payload=payload,
+        )
+        outbox_record.delete()
+
+
+class NoopReplicator(RelationReplicator):
+    """Noop replicator."""
+
+    def replicate(self, event: ReplicationEvent):
+        """Noop."""
+        pass
+
+
+class SeedingRelationApiDualWriteHandler:
+    """Class to handle Dual Write API related operations specific to the seeding process."""
+
+    _replicator: RelationReplicator
+    _role: Role
+    _current_role_relations: list[common_pb2.Relationship]
+
+    def __init__(self, replicator: Optional[RelationReplicator] = None):
+        _replicator = replicator if replicator else OutboxReplicator(None)
+
+    def replication_enabled(self):
+        """Check whether replication enabled."""
+        return settings.REPLICATION_TO_RELATION_ENABLED is True
+
+    def prepare_for_update(self, role: Role):
+        if not self.replication_enabled():
+            return
+        
+        self._role = role
+        self._replicator.record = role
+        self._current_role_relations = self._generate_relations_for_role(role)
+
+    def replicate_update_system_role(self, role: Role):
+        if not self.replication_enabled():
+            return
+        
+        self._replicate(ReplicationEventType.UPDATE_SYSTEM_ROLE, self._current_role_relations, self._generate_relations_for_role(role))
+        
+    def replicate_new_system_role(self, role: Role):
+        if not self.replication_enabled():
+            return
+        self._replicate(ReplicationEventType.CREATE_SYSTEM_ROLE, list[common_pb2.Relationship], self._generate_relations_for_role(role))
+
+    def replicate_deleted_system_role(self, role: Role):
+        if not self.replication_enabled():
+            return
+        self._replicate(ReplicationEventType.DELETE_SYSTEM_ROLE, self._generate_relations_for_role(role), list[common_pb2.Relationship])
+    
+    def _generate_relations_for_role(self, role: Role) -> list[common_pb2.Relationship]:
+        if role.admin_default:
+            pass
+        if role.platform_default:
+            pass
+
+    def _replicate(self, event_type: str, remove: list[common_pb2.Relationship], add: list[common_pb2.Relationship]):
+        if not self.replication_enabled():
+            return
+        try:
+            self._replicator.replicate(
+                ReplicationEvent(
+                    type=self.event_type,
+                    # TODO: need to think about partitioning
+                    # Maybe resource id
+                    partition_key="rbactodo",
+                    remove=remove,
+                    add=add,
+                ),
+            )
+        except Exception as e:
+            raise DualWriteException(e)
+        
+
+>>>>>>> c5697871 (More seeding replicator)
 class RelationApiDualWriteHandler:
     """Class to handle Dual Write API related operations."""
 
