e9d4bd48deb17bcb85a4f1667011d7a960a43da0
Libor Pichler <lpichler@redhat.com>
2024-10-17 16:31:28 +0200

    Consolidate replication for customer default group

--- rbac/management/group/view.py
+++ rbac/management/group/view.py
@@ -1041,8 +1041,8 @@ class GroupViewSet(
             if serializer.is_valid(raise_exception=True):
                 roles = request.data.pop(ROLES_KEY, [])
             with transaction.atomic():
-                group = set_system_flag_before_update(group, request.tenant, request.user)
-                add_roles(group, roles, request.tenant, user=request.user)
+                group, relations = set_system_flag_before_update(group, request.tenant, request.user)
+                add_roles(group, roles, request.tenant, user=request.user, relations=relations)
             response_data = GroupRoleSerializerIn(group)
         elif request.method == "GET":
             serialized_roles = self.obtain_roles(request, group)
@@ -1063,7 +1063,7 @@ class GroupViewSet(
             role_ids = request.query_params.get(ROLES_KEY, "").split(",")
             serializer = GroupRoleSerializerIn(data={"roles": role_ids})
             if serializer.is_valid(raise_exception=True):
-                group = set_system_flag_before_update(group, request.tenant, request.user)
+                group, relations = set_system_flag_before_update(group, request.tenant, request.user)
                 remove_roles(group, role_ids, request.tenant, request.user)
 
             return Response(status=status.HTTP_204_NO_CONTENT)
