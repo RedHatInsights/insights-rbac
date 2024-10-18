b357883961f55e7f1236947c06d749dbc659423d
Libor Pichler <lpichler@redhat.com>
2024-10-18 14:34:54 +0200

    Log relations optionaly from outbox replicator

--- rbac/management/relation_replicator/relation_replicator.py
+++ rbac/management/relation_replicator/relation_replicator.py
@@ -49,6 +49,7 @@ class ReplicationEventType(str, Enum):
     MIGRATE_CUSTOM_ROLE = "migrate_custom_role"
     MIGRATE_TENANT_GROUPS = "migrate_tenant_groups"
 
+
 class ReplicationEvent:
     """What tuples changes to replicate."""
 
