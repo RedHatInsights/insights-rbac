f973821b0eef7b478d924d0f751a756fd2ae7886
Alec Henninger <alechenninger@gmail.com>
2024-11-06 14:02:29 -0500

    Warn when trying to replicate an empty replication event

--- tests/management/relation_replicator/test_outbox_replicator.py
+++ tests/management/relation_replicator/test_outbox_replicator.py
@@ -57,3 +57,22 @@ class OutboxReplicatorTest(TestCase):
             {"relations_to_add": [json_format.MessageToDict(principal_to_group)], "relations_to_remove": []},
         )
         self.assertEqual(logged_event.aggregatetype, "relations-replication-event")
+
+    def test_replicate_empty_event_warns_instead_of_saving(self):
+        """Test replicate with empty event warns."""
+        event = ReplicationEvent(
+            add=[],
+            remove=[],
+            event_type=ReplicationEventType.ADD_PRINCIPALS_TO_GROUP,
+            info={"key": "value"},
+            partition_key=PartitionKey.byEnvironment(),
+        )
+
+        with self.assertLogs("management.relation_replicator.outbox_replicator", level="WARNING") as logs:
+            self.replicator.replicate(event)
+
+        self.assertEqual(len(self.log), 0)
+        self.assertEqual(len(logs.output), 1)
+        self.assertIn("Skipping empty replication event.", logs.output[0])
+        self.assertIn("info.key='value'", logs.output[0])
+        self.assertIn(str(ReplicationEventType.ADD_PRINCIPALS_TO_GROUP), logs.output[0])
