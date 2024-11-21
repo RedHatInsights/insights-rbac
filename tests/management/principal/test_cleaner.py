a469c2ee44fa3f944b5f1d5ffb8305d59e04a129
Alec Henninger <alechenninger@gmail.com>
2024-11-20 20:55:48 -0500

    Improve the test case to actually excercise the lock

--- tests/management/principal/test_cleaner.py
+++ tests/management/principal/test_cleaner.py
@@ -16,17 +16,19 @@
 #
 """Test the principal cleaner."""
 from functools import partial
+from threading import Event, Thread
 import uuid
 
 from unittest.mock import MagicMock, patch
 
+from django.db import connections, transaction
 from django.test import override_settings
 from rest_framework import status
 
 from management.group.definer import seed_group
 from management.group.model import Group
 from management.policy.model import Policy
-from management.principal.cleaner import clean_tenant_principals
+from management.principal.cleaner import LOCK_ID, clean_tenant_principals
 from management.principal.model import Principal
 from management.principal.cleaner import process_principal_events_from_umb
 from management.principal.proxy import external_principal_to_user
@@ -516,25 +518,49 @@ class PrincipalUMBTests(IdentityRequest):
         },
     )
     @patch("management.principal.cleaner.UMB_CLIENT")
-    @patch("management.principal.cleaner._lock_listener")
-    def test_does_not_update_user_if_lock_not_acquired(self, lock, client_mock, proxy_mock):
+    def test_does_not_update_user_if_lock_not_acquired(self, client_mock, proxy_mock):
         """Test that we can run principal creation event."""
-        lock.return_value = False
-        client_mock.canRead.return_value = True
-        client_mock.receiveFrame.return_value = MagicMock(body=FRAME_BODY_CREATION)
 
-        public_tenant = Tenant.objects.get(tenant_name="public")
-        Group.objects.create(name="default", platform_default=True, tenant=public_tenant)
-        tenant = Tenant.objects.get(org_id="17685860")
-        Principal.objects.create(tenant=tenant, username="principal-test")
+        def acquire_lock(ready: Event, close: Event):
+            """Simulates another process acquiring the lock."""
+            try:
+                conn = connections.create_connection("default")
+                conn.set_autocommit(False)
+                with conn.cursor() as cursor:
+                    cursor.execute("SELECT pg_try_advisory_xact_lock(%s);", [LOCK_ID])
+                    assert cursor.fetchone()[0] is True
+                    ready.set()
+                    close.wait(30)
+            finally:
+                conn.close()
 
-        process_principal_events_from_umb()
+        ready = Event()
+        close = Event()
 
-        client_mock.receiveFrame.assert_called_once()
-        client_mock.disconnect.assert_called_once()
-        client_mock.ack.assert_not_called()
+        try:
+            # Start the parallel "process"
+            other_thread = Thread(target=acquire_lock, args=(ready, close))
+            other_thread.start()
+            ready.wait(30)
 
-        self.assertFalse(Principal.objects.filter(user_id=self.principal_user_id).exists())
+            client_mock.canRead.side_effect = [True, False]
+            client_mock.receiveFrame.return_value = MagicMock(body=FRAME_BODY_CREATION)
+
+            public_tenant = Tenant.objects.get(tenant_name="public")
+            Group.objects.create(name="default", platform_default=True, tenant=public_tenant)
+            tenant = Tenant.objects.get(org_id="17685860")
+            Principal.objects.create(tenant=tenant, username="principal-test")
+
+            process_principal_events_from_umb()
+
+            client_mock.receiveFrame.assert_called_once()
+            client_mock.disconnect.assert_called_once()
+            client_mock.ack.assert_not_called()
+
+            self.assertFalse(Principal.objects.filter(user_id=self.principal_user_id).exists())
+        finally:
+            close.set()
+            other_thread.join()
 
 
 @override_settings(V2_BOOTSTRAP_TENANT=True, PRINCIPAL_CLEANUP_UPDATE_ENABLED_UMB=True)
