9793f5c95532a9bb2b52c6b9db8c0c584c922614
Keith Walsh <kwalsh@redhat.com>
2024-11-26 09:36:35 -0500

    Fix tenant query and tests in migrator
    `Tenant.objects.exclude(tenant_name="public", ready=False)` will only exclude tenants
    where `tenant_name="public"` AND `ready=False`, meaning the public tenant will
    always be returned.
    
    This updates that query and adds `ready=True` to the tests which create tenants.

--- rbac/migration_tool/migrate.py
+++ rbac/migration_tool/migrate.py
@@ -16,7 +16,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import logging
-from os import read
 from typing import Iterable
 
 from django.conf import settings
@@ -189,7 +188,7 @@ def migrate_data(exclude_apps: list = [], orgs: list = [], write_relationships: 
         return
 
     count = 0
-    tenants = Tenant.objects.exclude(tenant_name="public", ready=False)
+    tenants = Tenant.objects.filter(ready=True).exclude(tenant_name="public")
     replicator = _get_replicator(write_relationships)
     if orgs:
         tenants = tenants.filter(org_id__in=orgs)
