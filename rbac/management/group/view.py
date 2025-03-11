7fd8385e464dff14616bf3495431d70bd01cd69e
EvanCasey13 <evancasey2003@gmail.com>
2025-03-11 11:57:00 +0000

    remove unused variables

--- rbac/management/group/view.py
+++ rbac/management/group/view.py
@@ -102,9 +102,6 @@ VALID_GROUP_PRINCIPAL_FILTERS = ["principal_username"]
 VALID_PRINCIPAL_ORDER_FIELDS = ["username"]
 VALID_PRINCIPAL_TYPE_VALUE = ["service-account", "user"]
 VALID_ROLE_ROLE_DISCRIMINATOR = ["all", "any"]
-USER_KEY = "user"
-SA_KEY = "service-account"
-ALL_KEY = "all"
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
