62a6dfbdd9f2ca13c42ce1021cb52ad395149a16
Daniel Agbay <dagbay@redhat.com>
2025-03-14 16:40:31 -0500

    add functionality to disable auth for internal apis

--- rbac/internal/middleware.py
+++ rbac/internal/middleware.py
@@ -38,12 +38,21 @@ class InternalIdentityHeaderMiddleware(MiddlewareMixin):
     """Middleware for the internal identity header."""
 
     header = RH_IDENTITY_HEADER
+    
+    def __is_no_auth(self, request):
+        no_auth_list = settings.INTERNAL_API_NO_AUTH_PATHS
+        no_auth = any(no_auth_path in request.path for no_auth_path in no_auth_list)
+        return no_auth
 
     def process_request(self, request):
         """Process request for internal identity middleware."""
         if not any([request.path.startswith(prefix) for prefix in settings.INTERNAL_API_PATH_PREFIXES]):
             # We are not in an internal API section
             return
+        
+        if self.__is_no_auth(request):
+            return
+        
         try:
             _, json_rh_auth = extract_header(request, self.header)
         except (JSONDecodeError, binascii.Error, KeyError):
