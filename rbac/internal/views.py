618f99a869df854729656cc67ef23f90496912f4
EvanCasey13 <evancasey2003@gmail.com>
2025-03-28 12:53:08 +0000

    Host none check

--- rbac/internal/views.py
+++ rbac/internal/views.py
@@ -97,7 +97,10 @@ PROXY = PrincipalProxy()
 redis_client = redis.StrictRedis(host="localhost", port=6379, decode_responses=True)
 
 # Create http client
-conn = http.client.HTTPSConnection(HOST)
+if HOST is not None:
+    conn = http.client.HTTPSConnection(HOST)
+else:
+    raise ValueError("HOST is None. A valid host is required.")
 
 token = get_jwt_from_redis(redis_client, conn, grant_type, client_id, client_secret, scopes, url)
 
