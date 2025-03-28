b44b43fd59717c8062cffda5d7f730b5a57a2f2e
EvanCasey13 <evancasey2003@gmail.com>
2025-03-28 13:23:02 +0000

    split conn and host checks

--- rbac/internal/views.py
+++ rbac/internal/views.py
@@ -99,9 +99,9 @@ redis_client = redis.StrictRedis(host="localhost", port=6379, decode_responses=T
 # Create http client
 if HOST is not None:
     conn = http.client.HTTPSConnection(HOST)
+
+if conn is not None:
     token = get_jwt_from_redis(redis_client, conn, grant_type, client_id, client_secret, scopes, url)
-else:
-    conn = None
 
 relation_api_gRPC_server = relations_api_server
 
