f1fd8f901fa7f6cba0ba0fed28a53c0abb4c4678
Petra Číhalová <pcihalov@redhat.com>
2025-07-01 12:20:23 +0200

    update _enforce_hierarchy_depth_for_descendants() to use new parent id instead of workspace object

--- rbac/management/workspace/service.py
+++ rbac/management/workspace/service.py
@@ -104,11 +104,10 @@ class WorkspaceService:
 
     def move(self, instance: Workspace, new_parent_id: uuid.UUID) -> None:
         """Move a workspace under new parent."""
-        new_parent_workspace = Workspace.objects.get(id=new_parent_id)
         self._prevent_moving_non_standard_workspace(instance)
         self._prevent_moving_workspace_under_own_descendant(new_parent_id, instance)
         self._enforce_hierarchy_depth(new_parent_id, instance.tenant)
-        self._enforce_hierarchy_depth_for_descendants(new_parent_workspace, instance)
+        self._enforce_hierarchy_depth_for_descendants(new_parent_id, instance)
         # TODO: Implement actual move operation
 
     def _enforce_hierarchy_depth(self, target_parent_id: uuid.UUID, tenant: Tenant) -> None:
@@ -140,9 +139,9 @@ class WorkspaceService:
         return max_depth_for_workspace > settings.WORKSPACE_HIERARCHY_DEPTH_LIMIT
 
     @staticmethod
-    def _enforce_hierarchy_depth_for_descendants(new_parent_workspace: Workspace, instance: Workspace) -> None:
+    def _enforce_hierarchy_depth_for_descendants(new_parent_id: uuid.UUID, instance: Workspace) -> None:
         """Enforce the hierarchy depth for workspace descendant and target parent workspace."""
-        new_parent_depth = new_parent_workspace.ancestors().count()
+        new_parent_depth = Workspace.objects.get(id=new_parent_id).ancestors().count()
         workspace_tree_depth = instance.get_max_descendant_depth()
         total_depth = new_parent_depth + 1 + workspace_tree_depth
 
