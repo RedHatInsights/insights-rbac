b4ae19747752403eb83bb8910934cb494add0f69
EvanCasey13 <evancasey2003@gmail.com>
2025-07-16 09:38:56 +0100

    update grpc errors + tests

--- rbac/internal/views.py
+++ rbac/internal/views.py
@@ -1524,7 +1524,7 @@ def lookup_resource(request):
         return JsonResponse("No resource found", status=204, safe=False)
     except RpcError as e:
         logger.error(f"gRPC error: {str(e)}")
-        return JsonResponse({"detail": "Error occurred in gRPC call", "error": str(e)}, status=500)
+        return JsonResponse({"detail": "Error occurred in gRPC call", "error": str(e)}, status=400)
     except Exception as e:
         logger.error(f"Unexpected error: {str(e)}")
         return JsonResponse(
@@ -1584,7 +1584,7 @@ def read_tuples(request):
         return JsonResponse("No tuples found", status=204, safe=False)
     except RpcError as e:
         logger.error(f"gRPC error: {str(e)}")
-        return JsonResponse({"detail": "Error occurred in gRPC call", "error": str(e)}, status=500)
+        return JsonResponse({"detail": "Error occurred in gRPC call", "error": str(e)}, status=400)
     except Exception as e:
         logger.error(f"Unexpected error: {str(e)}")
         return JsonResponse({"detail": "Error occurred in call to read tuples endpoint", "error": str(e)}, status=500)
