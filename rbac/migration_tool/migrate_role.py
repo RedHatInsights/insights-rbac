6ec15f2971665f623a5325cb4b193bf601b40a97
Janet Cobb <jacobb@redhat.com>
2025-07-21 09:04:40 -0400

    Remove unused migrate_role

--- rbac/migration_tool/migrate_role.py
+++ rbac/migration_tool/migrate_role.py
@@ -18,10 +18,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from typing import Iterable
 
 from kessel.relations.v1beta1 import common_pb2
-from management.role.model import BindingMapping, Role
 from management.workspace.model import Workspace
 from migration_tool.models import V2rolebinding
-from migration_tool.sharedSystemRolesReplicatedRoleBindings import v1_role_to_v2_bindings
 
 
 def get_kessel_relation_tuples(
@@ -35,19 +33,3 @@ def get_kessel_relation_tuples(
         relationships.extend(v2_role_binding.as_tuples())
 
     return relationships
-
-
-def migrate_role(
-    role: Role,
-    default_workspace: Workspace,
-    current_bindings: Iterable[BindingMapping] = [],
-) -> tuple[list[common_pb2.Relationship], list[BindingMapping]]:
-    """
-    Migrate a role from v1 to v2, returning the tuples and mappings.
-
-    The mappings are returned so that we can reconstitute the corresponding tuples for a given role.
-    This is needed so we can remove those tuples when the role changes if needed.
-    """
-    v2_role_bindings = v1_role_to_v2_bindings(role, default_workspace, current_bindings)
-    relationships = get_kessel_relation_tuples([m.get_role_binding() for m in v2_role_bindings], default_workspace)
-    return relationships, v2_role_bindings
