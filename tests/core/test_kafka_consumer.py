fd254b14f876c86c8493797294113008d62e28ae
EvanCasey13 <evancasey2003@gmail.com>
2025-10-03 15:23:30 +0100

    test fix

--- tests/core/test_kafka_consumer.py
+++ tests/core/test_kafka_consumer.py
@@ -534,9 +534,9 @@ class RBACKafkaConsumerTests(TestCase):
 
         self.assertFalse(result)
 
-    @patch("api.models.Tenant")
     @patch("core.kafka_consumer.relations_api_replication._write_relationships")
-    def test_process_relations_message_success(self, mock_write_relationships, mock_tenant_model):
+    @patch("core.kafka_consumer.Tenant")
+    def test_process_relations_message_success(self, mock_tenant_model, mock_write_relationships):
         """Test successful relations message processing."""
         consumer = RBACKafkaConsumer()
 
