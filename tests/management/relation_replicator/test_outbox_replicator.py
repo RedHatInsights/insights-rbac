52e208e27fa10369f10dbfe49e5e83e4229d4f18
EvanCasey13 <evancasey2003@gmail.com>
2025-10-14 16:33:48 +0100

    remove context from test for group

--- tests/management/relation_replicator/test_outbox_replicator.py
+++ tests/management/relation_replicator/test_outbox_replicator.py
@@ -93,7 +93,6 @@ class OutboxReplicatorTest(TestCase):
                     json_format.MessageToDict(principal_to_group_remove1),
                     json_format.MessageToDict(principal_to_group_remove2),
                 ],
-                "resource_context": {"key": "value"},
             },
         )
         self.assertEqual(logged_event.aggregatetype, "relations-replication-event")
