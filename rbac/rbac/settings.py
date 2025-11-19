069b54a91aae33bd73141284ddc25373dac022ef
EvanCasey13 <evancasey2003@gmail.com>
2025-11-19 12:00:19 +0000

    lint fix

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -566,7 +566,9 @@ V2_APIS_ENABLED = ENVIRONMENT.bool("V2_APIS_ENABLED", default=False)
 V2_READ_ONLY_API_MODE = ENVIRONMENT.bool("V2_READ_ONLY_API_MODE", default=False)
 READ_ONLY_API_MODE = ENVIRONMENT.get_value("READ_ONLY_API_MODE", default=False)
 V1_ROLE_PERMISSION_BLOCK_LIST = [
-    permission.strip() for permission in ENVIRONMENT.get_value("V1_ROLE_PERMISSION_BLOCK_LIST", default="").split(",") if permission.strip()
+    permission.strip()
+    for permission in ENVIRONMENT.get_value("V1_ROLE_PERMISSION_BLOCK_LIST", default="").split(",")
+    if permission.strip()
 ]
 
 # Read-your-writes settings
