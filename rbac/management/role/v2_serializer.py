de7f5bf4d0484f0e2f410602b8f7a01aff5495c3
Jonathan Marcantonio <jmarcant@redhat.com>
2026-01-26 09:39:13 -0500

    Format files to pass lint check
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- rbac/management/role/v2_serializer.py
+++ rbac/management/role/v2_serializer.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Serializers for V2 Role API."""
+
 from dataclasses import dataclass, field
 from typing import Optional
 
