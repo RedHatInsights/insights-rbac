562c6dd439cb93f42b33304ea993f4878ae788e9
Janet Cobb <jacobb@redhat.com>
2026-02-24 14:27:18 -0500

    RHCLOUD-44659: Add API for running orphan removal in worker (#2525)
    * Abort orphan removal script after 10 failures
    
    * Add ability to limit tenants
    
    * Opportunistic fixes
    
    * Add internal API for running orphan removal script
    
    * Fix error message
    
    * Fix typo
    
    * More typo
    
    * wrong docs
    
    * Use keyword argument

--- rbac/management/management/commands/fix_orphan_relations.py
+++ rbac/management/management/commands/fix_orphan_relations.py
@@ -16,9 +16,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+import dataclasses
 import datetime
+import enum
+import itertools
 import logging
 from collections.abc import Iterable
+from typing import Optional
 
 from django.core.management import BaseCommand, CommandError
 from django.db.models import Q, QuerySet
@@ -30,6 +34,21 @@ from management.workspace.model import Workspace
 from api.models import Tenant
 
 logger = logging.getLogger(__name__)
+
+_abort_threshold = 10
+
+
+class _StopReason(enum.Enum):
+    SUCCESS = "success"
+    ABORTED = "aborted"
+
+
+@dataclasses.dataclass
+class _MigrateResult:
+    stop_reason: _StopReason
+    success_count: int
+    modified_count: int
+    failed_orgs: frozenset[str]
 
 
 class Command(BaseCommand):
@@ -50,6 +69,12 @@ class Command(BaseCommand):
             "--all",
             action="store_true",
             help="remove orphan relations for all tenants",
+        )
+
+        parser.add_argument(
+            "--tenant-limit",
+            type=int,
+            help="maximum number of tenants to process",
         )
 
     def _base_query(self) -> QuerySet:
@@ -80,29 +105,21 @@ class Command(BaseCommand):
 
         yield from base.exclude(pk__in=seen).distinct().iterator()
 
-    def handle(self, *args, **options):
-        """Execute the command."""
-        if not options["all"]:
-            raise CommandError(
-                "Must pass --all in order to remove orphan relations for all tenants. "
-                "(This is for forwards compatibility purposes.)",
-                returncode=2,
-            )
+    @staticmethod
+    def _try_migrate(tenants: Iterable[Tenant], estimated_count: int) -> _MigrateResult:
+        stop_reason = _StopReason.SUCCESS
 
         success_count = 0
         modified_count = 0
         failed_orgs = []
 
-        tenant_count = self._base_query().count()
-        logger.info(f"About to remove orphan relations for ~{tenant_count} tenants.")
-
-        for index, tenant in enumerate(self._prioritized_tenants()):
+        for index, tenant in enumerate(tenants):
             start_time = datetime.datetime.now(datetime.timezone.utc)
             modified = False
             failed = False
 
             logger.info(
-                f"Beginning migration of tenant {index + 1}/~{tenant_count} with org_id={tenant.org_id!r} "
+                f"Beginning migration of tenant {index + 1}/~{estimated_count} with org_id={tenant.org_id!r} "
                 f"at {start_time}"
             )
 
@@ -142,12 +159,55 @@ class Command(BaseCommand):
                 if modified:
                     modified_count += 1
 
-        logger.info(
-            f"Successfully removed orphan relations for {success_count} tenants, "
-            f"of which {modified_count} were modified."
+            if len(failed_orgs) >= _abort_threshold:
+                stop_reason = _StopReason.ABORTED
+                logger.error(f"Aborting after {_abort_threshold} tenants failed.")
+                break
+
+        return _MigrateResult(
+            stop_reason=stop_reason,
+            success_count=success_count,
+            modified_count=modified_count,
+            failed_orgs=frozenset(failed_orgs),
         )
 
-        if len(failed_orgs) > 0:
+    def _limited_tenants(self, limit: Optional[int]) -> tuple[Iterable[Tenant], int]:
+        base_count = self._base_query().count()
+
+        if limit is None:
+            return self._prioritized_tenants(), base_count
+
+        return itertools.islice(self._prioritized_tenants(), limit), min(base_count, limit)
+
+    def handle(self, *args, **options):
+        """Execute the command."""
+        limit: Optional[int] = options["tenant_limit"]
+
+        if (not options["all"]) and (limit is None):
             raise CommandError(
-                f"Failed to remove orphan relations tenants with the following org_ids: {failed_orgs}", returncode=1
+                "Must pass --all or --tenant-limit to specify how many tenants to process.",
+                returncode=2,
             )
+
+        if (limit is not None) and (limit <= 0):
+            raise CommandError("Limit must be positive.", returncode=2)
+
+        tenants, tenant_count = self._limited_tenants(limit=limit)
+
+        logger.info(f"About to remove orphan relations for ~{tenant_count} tenants.")
+
+        result = self._try_migrate(tenants=tenants, estimated_count=tenant_count)
+
+        logger.info(
+            f"Successfully removed orphan relations for {result.success_count} tenants, "
+            f"of which {result.modified_count} were modified."
+        )
+
+        if len(result.failed_orgs) > 0:
+            raise CommandError(
+                f"Failed to remove orphan relations tenants with the following org_ids: {result.failed_orgs}",
+                returncode=1,
+            )
+
+        if result.stop_reason != _StopReason.SUCCESS:
+            raise CommandError(f"Stopped for a reason other than success: {result.stop_reason.name}", returncode=1)
